9cb026d00f5189af2f3fc81d8661a04cf3592ffa
Brian Thompson <brianrt23@gmail.com>
2019-04-27 18:47:55 -0500

    Build

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'Ecosystem', // Title for your website.
   tagline: 'Documentation center for AwarEnv',
-  url: 'https://awarenv.tech/ecosystem', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://awarenv.github.io', // Your website URL
+  baseUrl: '/ecosystem/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
