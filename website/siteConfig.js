f5060d022f207644bc8e5dde9764ef6b4b3e7b6b
Brian Thompson <brianrt23@gmail.com>
2019-04-27 19:19:58 -0500

    Change root once and for all

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Ecosystem', // Title for your website.
   tagline: 'Documentation center for AwarEnv',
   url: 'https://awarenv.github.io', // Your website URL
-  baseUrl: '/projects/', // Base URL for your project */
+  baseUrl: '/ecosystem/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
