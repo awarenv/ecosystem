8236bdbc941a100bd791b4647a419fcf5d6439a6
Brian Thompson <brianrt23@gmail.com>
2019-04-27 19:06:56 -0500

    Try one more time

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,13 +24,13 @@ const siteConfig = {
   title: 'Ecosystem', // Title for your website.
   tagline: 'Documentation center for AwarEnv',
   url: 'https://awarenv.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/ecosystem/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'ecosystem',
+  projectName: 'home',
   organizationName: 'awarenv',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
